7123cb11538ea3ebd25bc557f06af1c675c31173
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-11-30 19:16:36 +0100

    Create webChallenge8.php

---
+++ webChallenge8.php
@@ -0,0 +1,9 @@
+<?php
+    $a = 15; #Dodjeljivanje vrijednosti promjenljivoj a
+    $povrsina = $a * $a; #Izračunavanje površine kvadrata
+    $obim = 4 * $a; #Izračunavanje obima kvadrata
+
+    echo $povrsina; #Ispisivanje površine kvadrata
+    echo "<br><br>"; #Dva prelaska u novi red
+    echo $obim; #Ispisivanje obima kvadrata
+?>
