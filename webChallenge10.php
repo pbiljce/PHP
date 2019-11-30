41acb133502de91ec9290aad4fab75bd5e29f555
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-11-30 19:37:50 +0100

    Create webChallenge10.php

---
+++ webChallenge10.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 13;
+    $b = 3;
+    $c = 17;
+
+    printf("Broj $a je u oktalnom sistemu broj %o.",$a);
+    echo "<br>";
+    printf("Broj $b je u heksadecimalnom sistemu broj %x.",$b);
+    echo "<br>";
+    printf("Broj $c je u binarnom sistemu broj %b.",$c);
+?>
