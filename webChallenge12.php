e24affe66e5b8949118fb2aa7b6248db96e79df3
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-11-30 20:24:49 +0100

    Update webChallenge12.php

--- webChallenge12.php
+++ webChallenge12.php
@@ -4,7 +4,7 @@
     $longituda = 18.3623065;
     $ocjenaRestorana = 5;
     $statusRestorana = "Otvoren";
-    $brojTelefona = +38765123456;
+    $brojTelefona = "+38765123456";
 
     echo "Restoran: " . $nazivRestorana . "<br>";
     echo "<a href='https://maps.google.com?$latituda,$longituda'>Lokacija restorana</a><br>";
