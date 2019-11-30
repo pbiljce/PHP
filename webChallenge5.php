45474c296f7da2575809234082dfd097697a544e
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-11-30 19:05:09 +0100

    Create webChallenge5.php

---
+++ webChallenge5.php
@@ -0,0 +1,13 @@
+<?php
+    $r = 2;
+    $PI = 3.14;
+    {
+        $area = $r * $r * $PI;
+    }
+    {
+        $perimeter = 2 * $r * $PI;
+    }
+    echo $area;
+    echo "<br/>";
+    echo $perimeter;
+?>
