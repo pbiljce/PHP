84bd0381be71e7b844c0682a0f4276fe2b58143f
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-01 14:12:49 +0100

    Create webChallenge22.php

---
+++ webChallenge22.php
@@ -0,0 +1,9 @@
+<?php
+    $currentDate = date ("d-m-Y");
+    $userAge = 25;
+    $userName = "John";
+
+    echo "Good morning, " . $userName . ", welcome to our site!" . PHP_EOL;
+    echo "Your age is, " . $userAge . "!" . PHP_EOL;
+    echo "Current date is, " . $currentDate . "!";
+?>
