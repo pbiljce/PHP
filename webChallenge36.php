cb61e1c0f2a79203b95ae9ebf7ffea5ea33e85c8
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-08 15:25:02 +0100

    Create webChallenge36.php

---
+++ webChallenge36.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>webChallenge36</title>
+</head>
+<body>
+    <?php
+        for($i = 1; $i <= 100; $i++){
+            $broj = pow($i,2);
+            echo $broj . "<br>";
+        }
+    ?>
+</body>
+</html>
