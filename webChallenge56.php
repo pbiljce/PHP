f972bc0dcb31c65e61dd5039c8072397c28e36e2
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-15 21:56:39 +0100

    Update webChallenge56.php

--- webChallenge56.php
+++ webChallenge56.php
@@ -1,7 +1,9 @@
 <?php
     $playersPerMatch = array(3,2,5,1,2,3,4,5);
     for($i = 0; $i < count($playersPerMatch); $i++){
-        $playerMatch[] = array_fill(0,$playersPerMatch[$i],"igrac");
+        $playerMatch[] = array_fill(0,$playersPerMatch[$i],"igrac");    
     }
+    echo "<pre>";
     print_r($playerMatch);
+    echo "</pre>";
 ?>
