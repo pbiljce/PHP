1f7c1c712cf675657aff5e551994d1f8a7cc3f70
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-15 10:28:20 +0100

    Create webChallenge46.php

---
+++ webChallenge46.php
@@ -0,0 +1,5 @@
+<?php
+    $niz = array("crveni","zeleni","bijele","plavo");
+
+    echo "Danas sam prolazio kroz Sarajevo. Bilo je zaista prelijepo. Vidio sam $niz[0] tepih, $niz[1] park, lijepe, $niz[2] golubove i zaista $niz[3] nebo."
+?>
