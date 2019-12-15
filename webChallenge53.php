538667cf69e1fe58e5c1f16a30f10cde65fd0211
pbiljce <57287222+pbiljce@users.noreply.github.com>
2019-12-15 14:05:25 +0100

    Create webChallenge53.php

---
+++ webChallenge53.php
@@ -0,0 +1,20 @@
+<?php
+    $niz = array(
+        array(1, 2, 3),
+        array(2, 2, 3),
+        array(3, 2, 3),
+    );
+
+    $niz1 = array(
+        array(1, 2, 3),
+        array(2, 2, 3),
+        array(3, 2, 3),
+    );
+
+    for($i = 0; $i < count($niz); $i++){
+        for($j = 0; $j < count($niz[$i]); $j++){
+            echo $niz[$i][$j] + $niz1[$i][$j];
+        }
+        echo "<br>";
+    }
+?>
